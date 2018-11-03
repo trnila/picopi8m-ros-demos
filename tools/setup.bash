62b9a4d1284ca9780e3f0f122a7a17d7d76aca2f
Daniel Trnka <daniel.trnka@gmail.com>
2018-11-03 22:14:01 +0100

    added tool scripts

---
+++ tools/setup.bash
@@ -0,0 +1,3 @@
+export PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd ):$PATH"
+export CMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH:/opt/freertos-tn/tools/cmake_toolchain_files/"
+export M4SDK_PATH="/opt/freertos-tn/"
