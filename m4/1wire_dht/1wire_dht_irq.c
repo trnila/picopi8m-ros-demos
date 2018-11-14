840d14308005c0fe813738306463588070e6011b
Daniel Trnka <daniel.trnka@gmail.com>
2018-11-14 21:24:07 +0100

    m4: 1wire_dht_irq simplified

--- m4/1wire_dht/1wire_dht_irq.c
+++ m4/1wire_dht/1wire_dht_irq.c
@@ -8,8 +8,17 @@
 #include "pin_mux.h"
 #include "clock_config.h"
 
+// dont forget to change irq function if you change GPIO port
+const int PIN = 26;
+
+// 2 irq for 8 bit in 5 bytes + 3 irqs in start sequence
+const int RECEIVE_COMPLETE = 2 * 8 * 5 + 3;
+
 volatile uint32_t tick;
 uint8_t data[5];
+volatile int state = 0;
+volatile uint32_t prev;
+volatile uint32_t times[80];
 
 void BOARD_InitPins(void) {
   IOMUXC_SetPinMux(IOMUXC_UART3_RXD_UART3_RX, 0U);
@@ -40,62 +49,39 @@ void delay(uint32_t usec) {
   while(tick - start < dest);
 }
 
-int recv(uint32_t timeout_usec, int state, uint32_t *duration) {
-  for(int i = 0; i < timeout_usec; i++) {
-    delay(1);
-    if(GPIO_PinRead(GPIO4, 26) == state) {
-      if(duration) {
-        *duration = i * 1;
-      }
-      return 1;
-    }
-  }
-
-  return 0;
-}
-
-volatile int state = 0;
-volatile uint32_t low;
-volatile uint32_t prev;
-volatile uint32_t times[240];
-int x = 0;
-
 void GPIO4_Combined_16_31_IRQHandler() {
-  GPIO_PortClearInterruptFlags(GPIO4, 1 << 26); // GPIO4->ISR = 1 << PIN;
-  int val = GPIO_PinRead(GPIO4, 26);
-  if(state == 0) {
-    if(val == 0) {
-      state++;
-    }
-  } else if(state == 1) {
-    if(val == 1) {
-      state++;
-    }
-  } else if(state == 2) {
-    if(val == 0) {
+  GPIO_PortClearInterruptFlags(GPIO4, 1 << PIN); // GPIO4->ISR = 1 << PIN;
+  int val = GPIO_PinRead(GPIO4, PIN);
+
+  if(state < 3) {
+    // complete start procedure
+    // wait for 0, then 1 then again 0
+    if((state & 1) == val) {
       state++;
       prev = tick;
-      x = 0;
     }
   } else {
-
     int i = state - 3;
     int b = i / 16;
     int diff = tick - prev;
     prev = tick;
 
-    if(i % 2 == 1) {
+    // if we are at falling edge, and high signal was bigger then 40us
+    // store it as a 1
+    if((i & 1) == 1) {
      data[b] <<= 1;
      if(diff > 40) {
         data[b] |= 1;
      } 
-    } else {
-      low = diff;
     }
 
     times[i] = diff;
-
     state++;
+
+    // 2 irq for 8 bit in 5 bytes + 3 irqs in start sequence
+    if(state >= RECEIVE_COMPLETE) {
+      GPIO_DisableInterrupts(GPIO4, 1 << PIN);
+    }
   }
 }
 
@@ -110,14 +96,14 @@ int main(void) {
   BOARD_InitDebugConsole();
   BOARD_InitMemory();
 
-  printf("dht22 example\r\n");
+  printf("dht22 irq example\r\n");
 
   // setup GPIO4_IO26
   gpio_pin_config_t conf;
   conf.direction = kGPIO_DigitalInput;
   conf.outputLogic = 0;
   conf.interruptMode = kGPIO_NoIntmode;
-  GPIO_PinInit(GPIO4, 26, &conf);
+  GPIO_PinInit(GPIO4, PIN, &conf);
 
   uint32_t period_us = 1;
   uint32_t ticks = SystemCoreClock / 1000000UL * period_us;
@@ -128,16 +114,35 @@ int main(void) {
 
 
   for(;;) {
-    // we should not measure more than once per 2 seconds
-    delay(2000 * 1000);
-    DisableIRQ(GPIO4_Combined_16_31_IRQn);
-    printf("%d\r\n", state);
+    // 1) pull pin low for at least 1 ms - we choosed 5 ms
+    conf.direction = kGPIO_DigitalOutput;
+    GPIO_PinInit(GPIO4, PIN, &conf);
+    delay(5000);
 
-    for(int i = 0; i < 80; i+=2) {
+    // 2) activate pull up and complete receive in interrupts
+    state = 0;
+    conf.direction = kGPIO_DigitalInput;
+    conf.interruptMode = kGPIO_IntRisingOrFallingEdge;
+    GPIO_PinInit(GPIO4, PIN, &conf);
+    GPIO_EnableInterrupts(GPIO4, 1 << PIN);
+    EnableIRQ(GPIO4_Combined_16_31_IRQn);
+
+    // wait for transmission
+    delay(2000 * 1000);
+    GPIO_DisableInterrupts(GPIO4, 1 << PIN);
+
+    // dump captured times from transmission
+    for(int i = 0; i < 80; i += 2) {
       if(i % 16 == 0 && i != 0) {
         printf("\r\n");
       }
       printf("%d %d   ", times[i], times[i + 1]);
+    }
+    printf("\r\n");
+
+    if(state != RECEIVE_COMPLETE) {
+      printf("failed to receive data from dht, current state %d\r\n", state);
+      continue;
     }
 
     // | RH high | RH low | T high | T low | checksum |
@@ -147,6 +152,7 @@ int main(void) {
       printf("checksum %x not matches computed %x\r\n", data[4], sum & 0xFF);
     }
 
+    printf("raw: ");
     for(int i = 0; i < 5; i++) {
       printf("%02x ", data[i]);
     }
@@ -160,20 +166,6 @@ int main(void) {
     for(int i = 0; i < 5; i++) {
       data[i] = 0x00;
     }
-    
-
-    // 1) pull pin low for at least 1 ms - we choosed 5 ms
-    conf.direction = kGPIO_DigitalOutput;
-    GPIO_PinInit(GPIO4, 26, &conf);
-    delay(5000);
-
-    // 2) activate pull up and wait up to 40 us for 0 from DHT
-    state = 0;
-    conf.direction = kGPIO_DigitalInput;
-    conf.interruptMode = kGPIO_IntRisingOrFallingEdge;
-    GPIO_PinInit(GPIO4, 26, &conf);
-    GPIO_EnableInterrupts(GPIO4, 1 << 26);
-    EnableIRQ(GPIO4_Combined_16_31_IRQn);
   }
 
   for(;;);
