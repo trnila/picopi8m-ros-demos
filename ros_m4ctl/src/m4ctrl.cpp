71e757d227a65a40d5745cae1f2d1323105b2882
Daniel Trnka <daniel.trnka@gmail.com>
2019-02-07 18:06:48 +0100

    renamed m4ctrl to ros_m4ctl It provides ros_m4ctl with Start/Stop service and m4ctl for local starting of m4 firmware

--- ros_m4ctl/src/m4ctrl.cpp
+++ ros_m4ctl/src/m4ctrl.cpp
@@ -1,25 +1,45 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "ros/ros.h"
 #include "m4ctrl/Start.h"
 #include "m4ctrl/Stop.h"
-#include "m4core.h"
 
-M4Core core;
+template<typename...Args>
+bool m4ctl(Args... args) {
+  pid_t child = fork();
+  if(child < 0) {
+    perror("fork:");
+    return false;
+  }
+
+  if(child == 0) {
+    execlp("m4ctl", "m4ctl", args..., NULL);
+    exit(1);
+  }
+  
+  int status;
+  waitpid(child, &status, 0);
+
+  return WEXITSTATUS(status) == 0;
+}
+
 
 bool start(m4ctrl::Start::Request &req, m4ctrl::Start::Response &res) {
   ROS_INFO("Boot firmware '%s' requested", req.firmware_path.c_str());
-  core.boot_firmware(req.firmware_path.c_str());
+  m4ctl("start", req.firmware_path.c_str());
   return true;
 }
 
 bool stop(m4ctrl::Stop::Request &req, m4ctrl::Stop::Response &res) {
   ROS_INFO("M4 stop requested");
-  core.stop();
+  m4ctl("stop");
   return true;
 }
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "m4ctrl");
-  ros::NodeHandle n("m4ctrl");
+  ros::init(argc, argv, "ros_m4ctl");
+  ros::NodeHandle n("ros_m4ctl");
 
   ros::ServiceServer startSrv = n.advertiseService("start", start);
   ros::ServiceServer stopSrv = n.advertiseService("stop", stop);
